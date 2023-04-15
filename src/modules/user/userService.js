d1625eb83d2efe87aaffed15023afd86278941bc
Stone <lukasr00ts@hotmail.com>
2023-04-15 18:39:09 -0300

    Criado login e autorizacao

--- src/modules/user/userService.js
+++ src/modules/user/userService.js
@@ -1,4 +1,8 @@
+import { sign } from 'jsonwebtoken' //function responsavel por gerar um token
+
 let users = [] // variavel temporaria enqt nao ultiza banco de dados
+
+const generateAcessToken = (data) => sign(data, 'secret')
 
 const getUserByEmail = (searchEmail) =>   // 
 users.find((obj) => obj.email === searchEmail) // users.find((obj) se achar retorna esse dado se n retorna undefined
@@ -6,10 +10,17 @@ users.find((obj) => obj.email === searchEmail) // users.find((obj) se achar reto
 
 
 export const signup = (data) => {
-  if (getUserByEmail(data.email)) {
-    console.log("EMAIL CADASTRADO")
-  } else {
+  if (getUserByEmail(data.email)) throw new Error('email_existente') // throw n deixa avançar o codigio ele pausa
+
     users.push(data) // no data se n tiver o email cadastrado ele cadastra
+    return generateAcessToken({ email: data.email })
   }
-  return true
+  
+export const login = (data) => {
+  const user = getUserByEmail(data.email)
+  if (!user) throw new Error('email_nao_encontrado')
+
+  if (user.password !== data.password) throw new Error('senha_incorreta')
+
+  return generateAcessToken({ email: data.email }) 
 }
